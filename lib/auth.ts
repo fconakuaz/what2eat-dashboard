cc546170da742776ae0123120cade5c22cbaa0a4
Francisco Nakú Acosta Zárate <fconakuaz@gmail.com>
2025-02-10 13:18:36 -0600

    Se agrega Auth de Google

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,11 @@
 import NextAuth from 'next-auth';
-import GitHub from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [GitHub]
+  providers: [GoogleProvider(
+    {
+      clientId: process.env.AUTH_GOOGLE_ID as string,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
+    }
+  )]
 });
--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,11 @@ const nextConfig = {
         protocol: 'https',
         hostname: '*.public.blob.vercel-storage.com',
         search: ''
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        search: ''
       }
     ]
   }
