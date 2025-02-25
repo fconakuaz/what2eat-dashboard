3557ca3b11681b562e3c90414eaadaaf136852cd
Francisco Nakú Acosta Zárate <fconakuaz@gmail.com>
2025-02-24 21:51:40 -0600

    Se crea servicio para obtener menú a compartir a otras personas sin usuario

---
+++ app/api/menu/[id]/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const id = await params?.id;
+  console.log('🚩🚩🚩 params 🚩🚩🚩');
+  console.log(id);
+  try {
+    if (!id) {
+      return NextResponse.json(
+        { error: 'El ID del menú es obligatorio' },
+        { status: 400 }
+      );
+    }
+
+    const menu = await prisma.savedMenu.findUnique({
+      where: { id }
+    });
+
+    if (!menu) {
+      return NextResponse.json(
+        { error: 'Menú no encontrado' },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({ success: true, menu });
+  } catch (error) {
+    console.error('Error al obtener el menú:', error);
+    return NextResponse.json(
+      { error: 'Error interno del servidor' },
+      { status: 500 }
+    );
+  }
+}
