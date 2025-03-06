9b1e54665ec68d778095bc90fb899551ee0c63f1
Francisco Nakú Acosta Zárate <fconakuaz@gmail.com>
2025-03-05 18:25:28 -0600

    Se oculta menu si no se ha iniciado sesión en modo desktop

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -22,7 +22,7 @@ export default async function DashboardLayout({
       <main className="flex min-h-screen w-full flex-col bg-muted/40">
         {session && <DesktopNav />}
         <div
-          className={`flex flex-col sm:gap-4 sm:py-4 pl-0 sm:${!session ? 'pl-0' : 'pl-14'}`}
+          className={`flex flex-col sm:gap-4 sm:py-4 pl-0 ${session?.user ? 'sm:pl-14' : 'sm:pl-0'}`}
         >
           <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
             <MobileNav />
