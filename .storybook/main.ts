d8b4ddb9df7d4899ee053e51bdb46c4017480746
Francisco Nakú Acosta Zárate <fconakuaz@gmail.com>
2025-03-14 11:59:36 -0600

    Se agrega Story de UI

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,20 @@
-import type { StorybookConfig } from "@storybook/experimental-nextjs-vite";
+import type { StorybookConfig } from '@storybook/experimental-nextjs-vite';
 
 const config: StorybookConfig = {
-  "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../stories/**/*.mdx',
+    '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)'
   ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test'
   ],
-  "framework": {
-    "name": "@storybook/experimental-nextjs-vite",
-    "options": {}
+  framework: {
+    name: '@storybook/experimental-nextjs-vite',
+    options: {}
   },
-  "staticDirs": [
-    "../public"
-  ]
+  staticDirs: ['../public']
 };
 export default config;
