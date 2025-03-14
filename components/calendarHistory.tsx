4204b46f9b8c879455a4cffb88b4a12e130004f1
Francisco Nakú Acosta Zárate <fconakuaz@gmail.com>
2025-03-14 11:08:44 -0600

    Se quita CalendarHistory por incompatibilidad con librerías de react19

--- components/calendarHistory.tsx
+++ components/calendarHistory.tsx
@@ -1,17 +1,18 @@
 'use client';
 
-import { Calendar } from '@/components/ui/calendar';
+// import { Calendar } from '@/components/ui/calendar';
 import { useCommonStore } from 'app/store/commonStore';
 
 export function CalendarHistory() {
   const { selectedDate, setDate } = useCommonStore();
 
   return (
-    <Calendar
-      mode="single"
-      selected={selectedDate}
-      onSelect={setDate}
-      className="rounded-md border shadow"
-    />
+    <></>
+    // <Calendar
+    //   mode="single"
+    //   selected={selectedDate}
+    //   onSelect={setDate}
+    //   className="rounded-md border shadow"
+    // />
   );
 }
